02ac301c3238948606448edb0e80c9bc1946b1bb
pallamganesh <pallamganesh213@gmail.com>
2024-04-08 23:05:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Home from "./Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Create from "./Create";
+import Update from "./Update";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/create" element={<Create />} />
+          <Route path="/edit/:id" element={<Update />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Data.jsx
@@ -0,0 +1,20 @@
+
+export const userList = [
+
+    {
+        name: "Ganesh Pallam",
+        email: "ganeshp@gmail.com",
+        id: 1
+    },
+    {
+        name: "Surendra Mekala",
+        email: "surendram@gmail.com",
+        id: 2
+    },
+    {
+        name: "Amarnath Palla",
+        email: "amar@gmail.com",
+        id: 3
+    }
+
+]
